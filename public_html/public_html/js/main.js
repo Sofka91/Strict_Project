9f52c763ee67b85946900909c7264ae3814e6b38
Sofija <sofijasokic91@gmail.com>
2020-03-11 18:35:15 +0100

    Project finished

--- public_html/public_html/js/main.js
+++ public_html/public_html/js/main.js
@@ -1,32 +1,28 @@
 
-$(document).ready(function(){
-    
-    
+$(document).ready(function () {
+
+
     //ANIMATION
     function animation() {
         var windowHeight = $(window).height();
         var scroll = $(window).scrollTop();
-        $('.animation').each(function() {
+        $('.animation').each(function () {
             let position = $(this).offset().top;
             let animation = $(this).attr('data-animation');
-	    let delay = $(this).attr('data-delay');
+            let delay = $(this).attr('data-delay');
             if (position < scroll + windowHeight - 100) {
-		$(this).css('animation-delay', delay);               
-		$(this).addClass(animation);               
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
             }
         });
     }
     animation();
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         animation();
     });
-    
-  
- 
 
-    
-    
-    
+
+
 });
 
 
